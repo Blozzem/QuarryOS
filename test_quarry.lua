078dc037cff31507b05e3ff437b76e6bc6640c5b
Blozzem <sperrtmichhd@gmail.com>
2026-07-20 22:06:06 +0200

    Create test_quarry.lua

---
+++ test_quarry.lua
@@ -0,0 +1,22 @@
+local quarry =
+    require("modules.quarry")
+
+
+print("================")
+print(" Quarry Test ")
+print("================")
+
+
+print("Räume Block vor mir...")
+
+
+quarry.clear()
+
+
+print("Fahre einen Block...")
+
+
+quarry.forward()
+
+
+print("Fertig")
