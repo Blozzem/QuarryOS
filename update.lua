4a4c5ca652bc8f1d2014420f3f30fb5bcd03df1a
Blozzem <sperrtmichhd@gmail.com>
2026-07-21 21:39:33 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,1 +1,47 @@
+--==========================================
+-- QuarryOS Updater
+-- Version 0.1
+--==========================================
 
+local BASE = "https://raw.githubusercontent.com/Blozzem/QuarryOS/main/"
+
+local files = {
+
+    "version.lua",
+
+    "core/config.lua",
+    "core/save.lua",
+    "core/job.lua",
+    "core/movement.lua",
+    "core/homepoint.lua",
+    "core/fuel.lua",
+    "core/inventory.lua",
+
+    "modules/quarry.lua",
+    "modules/miner.lua",
+
+    "setup.lua"
+
+}
+
+print("=================================")
+print("      QuarryOS Updater")
+print("=================================")
+print("")
+
+for _,file in ipairs(files) do
+
+    print("Update: "..file)
+
+    shell.run("delete "..file)
+
+    shell.run(
+        "wget "..BASE..file.." "..file
+    )
+
+end
+
+print("")
+print("=================================")
+print("Update abgeschlossen!")
+print("=================================")
