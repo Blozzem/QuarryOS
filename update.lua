1b1ff7f0e41679c5d34ce4c2126bd76c519342e0
Blozzem <sperrtmichhd@gmail.com>
2026-07-25 10:44:23 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,16 +1,44 @@
---=================================
--- QuarryOS Updater
---=================================
+--=====================================
+-- QuarryOS Updater v2
+--=====================================
 
 
 local BASE =
 "https://raw.githubusercontent.com/Blozzem/QuarryOS/main/"
 
 
-print("==============================")
-print("       QuarryOS Update")
-print("==============================")
+
+print("==========================")
+print("    QuarryOS Update v2")
+print("==========================")
 print("")
+
+
+
+------------------------------------------------
+-- Alte Manifest sichern
+------------------------------------------------
+
+if fs.exists("manifest.lua") then
+
+    fs.delete("manifest_old.lua")
+
+    fs.copy(
+        "manifest.lua",
+        "manifest_old.lua"
+    )
+
+    print("Alte Manifest gesichert")
+
+end
+
+
+
+------------------------------------------------
+-- Neue Manifest laden
+------------------------------------------------
+
+print("Lade neue Manifest...")
 
 
 shell.run(
@@ -18,27 +46,70 @@ shell.run(
 )
 
 
-local files =
+
+local newFiles =
     dofile("manifest.lua")
 
 
 
-for _,file in ipairs(files) do
+------------------------------------------------
+-- Alte Dateien löschen
+------------------------------------------------
+
+if fs.exists("manifest_old.lua") then
 
 
-    print("Update:")
-    print(file)
+    print("")
+    print("Entferne alte Dateien...")
 
 
-    local folder =
+    local oldFiles =
+        dofile("manifest_old.lua")
+
+
+    for _,file in ipairs(oldFiles) do
+
+
+        if fs.exists(file) then
+
+            print("Loesche "..file)
+
+            fs.delete(file)
+
+        end
+
+
+    end
+
+
+end
+
+
+
+------------------------------------------------
+-- Neue Dateien installieren
+------------------------------------------------
+
+
+print("")
+print("Installiere neue Dateien...")
+
+
+for _,file in ipairs(newFiles) do
+
+
+    local dir =
         fs.getDir(file)
 
 
-    if folder ~= "" then
+    if dir ~= "" then
 
-        fs.makeDir(folder)
+        fs.makeDir(dir)
 
     end
+
+
+    print("Lade "..file)
 
 
     shell.run(
@@ -51,6 +122,6 @@ end
 
 
 print("")
-print("==============================")
-print(" Update fertig!")
-print("==============================")
+print("==========================")
+print(" Update abgeschlossen!")
+print("==========================")
